5349a9aa839df2713ccda0232ffd6bb04786a5ce
quao627 <qua@mit.edu>
2025-09-23 00:07:58 -0400

    eval script

---
+++ run_eval.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+datasets=(
+    "complexor.json"
+    "industryor_test.json"
+    "mamo_complex_test.json"
+    "mamo_easy.json"
+    "nl4opt_test.json"
+    "nlp4lp_test.json"
+    "optibench.json"
+    "logior_test.json"
+)
+
+for dataset in "${datasets[@]}"; do
+    CUDA_VISIBLE_DEVICES=1 python eval.py --model_name_or_path /orcd/scratch/seedfund/001/multimodal/qua/huggingface/hub/ORLM-LLaMA-3-8B/ --tensor_parallel_size 1 --test_file ../baseline_test_data/$dataset --output_file ../results/orlm/$dataset --verbose
+done
