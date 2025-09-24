1f66ffe080b45c0031cdeff7cc9be3cdc9a93174
MikeZheng777 <hanzheng@mit.edu>
2025-09-24 23:05:15 +0000

    update

--- run_eval.sh
+++ run_eval.sh
@@ -14,7 +14,7 @@ datasets=(
 
 for dataset in "${datasets[@]}"; do
     # Using Qwen-7B model
-    CUDA_VISIBLE_DEVICES=1 python eval.py --model_name_or_path Qwen/Qwen-7B --tensor_parallel_size 1 --gpu_memory_utilization 0.9 --test_file ../baseline_test_data/$dataset --output_file results/Qwen-7B/$dataset --verbose
+    CUDA_VISIBLE_DEVICES=1 python eval.py --model_name_or_path Qwen/Qwen2-7B-Instruct --tensor_parallel_size 1 --gpu_memory_utilization 0.9 --test_file ../baseline_test_data/$dataset --output_file results/Qwen2-7B-Instruct/$dataset --verbose
     # CUDA_VISIBLE_DEVICES=1 python eval.py --model_name_or_path meta-llama/Meta-Llama-3-8B-Instruct --tensor_parallel_size 1 --gpu_memory_utilization 0.8 --test_file ../baseline_test_data/$dataset --output_file results/Meta-Llama-3-8B-Instruct/$dataset --verbose
     # CUDA_VISIBLE_DEVICES=1 python eval.py --model_name_or_path /orcd/scratch/seedfund/001/multimodal/qua/huggingface/hub/models--AlphaOpt_ORLM_Meta-Llama-3-8B-Instruct_ft --tensor_parallel_size 1 --gpu_memory_utilization 0.8 --test_file ../baseline_test_data/$dataset --output_file results/AlphaOpt_ORLM_Meta-Llama-3-8B-Instruct_ft_new/$dataset --verbose
 
