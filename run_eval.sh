63cdf3ab8d57469b2a6f082ff36d5a47f6f8aaad
MikeZheng777 <hanzheng@mit.edu>
2025-09-24 22:57:41 +0000

    update prompt

--- run_eval.sh
+++ run_eval.sh
@@ -5,16 +5,16 @@ datasets=(
 #    "industryor_test.json"
 #    "mamo_complex_test.json"
 #    "mamo_easy.json"
-#    "nl4opt_test.json"
+   "nl4opt_test.json"
 #    "nlp4lp_test.json"
 #    "optibench.json"
 #    "logior_test.json"
-     "logior.json"
+    #  "logior.json"
 )
 
 for dataset in "${datasets[@]}"; do
     # Using Qwen-7B model
-    CUDA_VISIBLE_DEVICES=1 python eval.py --model_name_or_path Qwen/Qwen-7B --tensor_parallel_size 1 --gpu_memory_utilization 0.8 --test_file ../baseline_test_data/$dataset --output_file results/Qwen-7B/$dataset --verbose
+    CUDA_VISIBLE_DEVICES=1 python eval.py --model_name_or_path Qwen/Qwen-7B --tensor_parallel_size 1 --gpu_memory_utilization 0.9 --test_file ../baseline_test_data/$dataset --output_file results/Qwen-7B/$dataset --verbose
     # CUDA_VISIBLE_DEVICES=1 python eval.py --model_name_or_path meta-llama/Meta-Llama-3-8B-Instruct --tensor_parallel_size 1 --gpu_memory_utilization 0.8 --test_file ../baseline_test_data/$dataset --output_file results/Meta-Llama-3-8B-Instruct/$dataset --verbose
     # CUDA_VISIBLE_DEVICES=1 python eval.py --model_name_or_path /orcd/scratch/seedfund/001/multimodal/qua/huggingface/hub/models--AlphaOpt_ORLM_Meta-Llama-3-8B-Instruct_ft --tensor_parallel_size 1 --gpu_memory_utilization 0.8 --test_file ../baseline_test_data/$dataset --output_file results/AlphaOpt_ORLM_Meta-Llama-3-8B-Instruct_ft_new/$dataset --verbose
 
