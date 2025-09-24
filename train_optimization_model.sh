699a09269aa6200ade588dafb58aecafe6f2d36b
MikeZheng777 <hanzheng@mit.edu>
2025-09-24 01:34:26 +0000

    code update

--- train_optimization_model.sh
+++ train_optimization_model.sh
@@ -12,7 +12,7 @@ MODEL_NAME_OR_PATH="meta-llama/Meta-Llama-3-8B"  # LLaMA-3-8B model
 # DATA_PATH="train_test_data/converted_data_messages_new_100.json"  # Your converted data
 DATA_PATH="train_test_data/converted_data_prompt_completion_new_100.json"  # Your converted data
 ## use the base model name and add the new model name
-SAVE_PATH="/orcd/scratch/seedfund/001/multimodal/qua/huggingface/hub/models--AlphaOpt_ORLM" + "Llama-3-8B_ft" 
+SAVE_PATH="/orcd/scratch/seedfund/001/multimodal/qua/huggingface/hub/models--AlphaOpt_ORLM_Meta-Llama-3-8B_ft"
 
 # Training Configuration
 NUM_GPUS=2  # Number of H200 GPUs available
