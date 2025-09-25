b1bd377bb79ff6508d351d69118e06c119be5c1c
MikeZheng777 <hanzheng@mit.edu>
2025-09-25 00:24:25 +0000

    code update

--- run_eval_multi_models.sh
+++ run_eval_multi_models.sh
@@ -20,6 +20,6 @@ models=(
 
 for model in "${models[@]}"; do
     for dataset in "${datasets[@]}"; do
-        CUDA_VISIBLE_DEVICES=1 python eval.py --model_name_or_path /orcd/scratch/seedfund/001/multimodal/qua/huggingface/hub/models--AlphaOpt_ORLM_Qwen2-7B-Instruct_ft_new_$model --tensor_parallel_size 1 --gpu_memory_utilization 0.8 --test_file ../baseline_test_data/$dataset --output_file results/AlphaOpt_ORLM_Qwen2-7B-Instruct_ft_new_$model/$dataset --verbose
+        CUDA_VISIBLE_DEVICES=1 python eval.py --model_name_or_path /orcd/scratch/seedfund/001/multimodal/qua/huggingface/hub/models--AlphaOpt_ORLM_Qwen2-7B-Instruct_ft_new_$model --tensor_parallel_size 1 --gpu_memory_utilization 0.8 --test_file ../baseline_test_data/$dataset --output_file results/AlphaOpt_ORLM_Qwen2-7B-Instruct_ft_new_$model/$dataset
     done
 done
